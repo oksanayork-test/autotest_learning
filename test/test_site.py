90af728301394a4ddd7c75036b5dcc1037fd12c7
oksanayork-test <teenagedream.k@gmail.com>
2026-09-11 11:09:56 +0300

    tests are ready

---
+++ test/test_site.py
@@ -0,0 +1,24 @@
+import time
+from pages.homepage import HomePage
+from pages.product import ProductPage
+
+
+def test_open_s6(browser):    
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_galaxy_s6()
+    product_page = ProductPage(browser)
+    product_page.check_title_is('Samsung galaxy s6')
+
+
+def test_monitors(browser):
+    homepage = HomePage(browser)
+    homepage.open()
+    # browser.get('https://www.demoblaze.com/index.html')
+    homepage.click_monitor()
+    # monitor_link = browser.find_element(By.CSS_SELECTOR, value='''[onclick="byCat('monitor')"]''')
+    # monitor_link.click()
+    time.sleep(2)
+    homepage.check_product_count(2)
+    # monitors = browser.find_elements(By.CSS_SELECTOR, value='.card')
+    # assert len(monitors) == 2
